5419c285f1a78341fbdb238ec918f1b168d6282a
Sapphire Doulas <145626933+Lacedaemonian88@users.noreply.github.com>
2024-05-09 00:19:52 +0000

    started project, doing poorly lol

---
+++ project.rb
@@ -0,0 +1,44 @@
+unpredictable_inputs = [
+  "Hello!",
+  Time.now,
+  rand(100),
+  :GOODBYE,
+  nil,
+  true,
+  false,
+  { :city => "Chicago", :state => "IL", :zip => 60654 }
+]
+
+some_random_input = unpredictable_inputs.sample
+# write your program below
+ some_random_input.each do |thing|
+  prompts = thing.split("")
+    if prompts.class == String
+      pp prompts.downcase
+    elsif 
+      prompts.class == Integer
+      if prompts.even?
+        pp "#{prompts} is even"
+      else prompts odd?
+        pp "#{prompts} is odd"
+      end
+    elsif
+       prompts.class == nil
+      pp "no object provided"
+    elsif 
+      prompts.class == Time
+      pp Time.now.strftime("%A").downcase 
+    # elsif 
+    #   prompts.class == Symbol
+      pp "#{prompts}".downcase
+    elsif 
+      prompts == true
+      pp "you may pass"
+    elsif 
+      prompts == false
+      pp "you may not pass"
+    else
+      pp prompts.keys
+  end
+ end
+pp some_random_input
